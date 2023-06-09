4626a665bc5b137d9da5cf3c397b0c81e72dbf87
Deez <73635465+collotebik@users.noreply.github.com>
2023-06-09 17:11:06 +0300

    initial

--- Mpesa-STK-Push-with-AJAX-master/index.php
+++ Mpesa-STK-Push-with-AJAX-master/index.php
@@ -29,16 +29,14 @@
                         </div>
 
                         <div class="form-group">
-                            <p class="text-warning mb-0">Amount</p> <input type="text" name="amount" placeholder="" size="17" id="amount" disabled value="4500">
+                            <p class="text-warning mb-0">Amount</p> <input type="text" name="amount" placeholder="" size="17" id="amount" disabled value="1">
                         </div>
                         <div class="form-group pt-2">
                             <div class="row d-flex">
-                            <div class="product">
 
                                 <div class="col-sm-7 pt-0">
                                     <button type="submit" class="btn btn-primary" id="pay" class="pay-button" name="pay"> Pay<i class="fas fa-arrow-right px-3 py-2"></i></button>
                                 </div>
-                            </div>
                             </div>
                         </div>
                     </form>
